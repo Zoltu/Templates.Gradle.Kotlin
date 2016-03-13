da26a3c6652cbdbf311a740034d656d2620beeec
Micah Zoltu <micah@zoltu.net>
2016-03-13 11:56:54 -0700

    Initial commit.

---
+++ src/main/kotlin/magic.group.magic/Main.kt
@@ -0,0 +1,9 @@
+package magic.group.magic
+
+fun main(args: Array<String>) {
+	println(getGreeting())
+}
+
+fun getGreeting(): String {
+	return "Hello world!"
+}
---
+++ src/test/kotlin/magic.group.magic/MainTests.kt
@@ -0,0 +1,11 @@
+package magic.group.magic
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class MainTests {
+	@Test
+	fun `getGreeting returns "Hello world!"`() {
+		assertEquals("Hello world!", getGreeting())
+	}
+}
